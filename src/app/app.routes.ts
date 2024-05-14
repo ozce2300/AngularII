1e07984d97b076d0dd8cfe717e855f9bea6ef9ab
Özgur Celik <ozce2300@student.miun.se>
2024-05-14 19:09:02 +0200

    Skapat filter metod

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { CourseComponent } from './course/course.component';
 
 export const routes: Routes = [
-    { path: "course", component: CourseComponent},
+    { path: "course", component: CourseComponent, title: "Kurser"},
     { path: "**", redirectTo: "course"}
 ];
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Course } from '../modules/course';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
@@ -14,16 +15,25 @@ export class CourseComponent {
   //Properties
 
   courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  filteredValue: string = "";
 
   constructor(private courseService : CourseService) {}
 
+//Method  
   ngOnInit() {
     this.courseService.getData().subscribe((courses) =>{
       this.courses = courses;
-    }
+      this.filteredCourses = courses;
+    });
+  }
 
-
-    );
+  applyfilter(): void {
+    this.filteredCourses = this.courses.filter((course) =>
+      course.code.toLowerCase().includes(this.filteredValue.toLowerCase()) ||
+      course.coursename.toLowerCase().includes(this.filteredValue.toLowerCase()) ||
+      course.progression.toLowerCase().includes(this.filteredValue.toLowerCase())
+)
   }
 
 }
